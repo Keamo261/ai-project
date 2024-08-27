2792b4a08e65f05fc39ac72a9f321509a3a0033d
Keamo261 <keamogetswe261@gmail.com>
2024-08-27 21:42:58 +0200

    Updated project design

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let apiKey = "21a849d40ft0a45f399f9odf6fa4fb86";
   let prompt = `User instructions: Generate a poem about ${instructionsInput}`;
   let context =
-    "You are an expert in poems about the night. Generate an 8 line poem about the night. Separate each line with a <br />. Make sure to follow the user instructions.";
+    "You are an expert in poems about the night. Generate an 6 line poem about the night. Separate each line with a <br />. Make sure to follow the user instructions.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let poemElement = document.querySelector("#poem");
